6222fb6cd1776b1dd7e520703d99ed87e7767f46
Seth Gates <1sethgates@gmail.com>
2023-06-27 12:30:43 -0700

    First commit of AWS Files

---
+++ EKSCanaryDeployment/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Welcome to our Stable page 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1 align="center">Hi 👋, I'm Seth</h1>
+<h3 align="center">A motivated Developer from the US!</h3>
+
+# This repository contains various types of code pertaining to usage with AWS.
